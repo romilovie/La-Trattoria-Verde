ce2204507396ad9a592209b92b3d8831c21af226
romilovie <113263553+romilovie@users.noreply.github.com>
2026-02-10 22:35:52 +0200

    Delete js.gitkeep directory

--- js.gitkeep/contacts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    alert('Спасибо! Ваше сообщение отправлено.');
-    this.reset();
-});
--- js.gitkeep/news.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.news-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        const card = button.closest('.news-card');
-        card.classList.toggle('open');
-        button.textContent = card.classList.contains('open')
-            ? 'Свернуть'
-            : 'Подробнее';
-    });
-});
